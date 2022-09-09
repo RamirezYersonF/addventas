d628d497256a3cd3a78c2517da6eb7be9a52c18e
Yerson Felipe Ramirez <yfrg10@gmail.com>
2022-09-09 18:36:39 -0500

    v.1

---
+++ appventas/appventas/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using appventas.Services;
+using appventas.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace appventas
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new iniciarsesion();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ appventas/appventas/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using appventas.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace appventas.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
